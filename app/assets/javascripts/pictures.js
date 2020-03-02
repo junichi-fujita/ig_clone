43ef4d9be3d32f4432904fbc5883fa0986795f63
junichi-fujita <fujita0909j@gmail.com>
2020-03-02 21:28:25 +0900

    controller picturesの作成

---
+++ app/assets/javascripts/pictures.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("a"),click(function(){
+    $("body").append('<div id="bg'>);
+
+    $("body").append('<div id="photo">');
+
+    $("#bg").hede();
+    $("#photo").hide();
+
+    return false;
+  })
+})
---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,51 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if params[:back]
+      render "top/home"
+    else
+      if @picture.save
+        redirect_to pictures_path, notice: "画僧を投稿しました。"
+      else
+        render "new"
+      end
+    end
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:text, :image, :image_cache)
+  end
+
+end
