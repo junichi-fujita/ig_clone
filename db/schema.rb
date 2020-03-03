0ef7fd671978e5889f9d26276b77b64036f8b329
junichi-fujita <fujita0909j@gmail.com>
2020-03-03 22:07:50 +0900

    userとpictureの１対多の関係を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_131301) do
+ActiveRecord::Schema.define(version: 2020_03_03_130006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_131301) do
     t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_03_02_131301) do
     t.string "password_digest"
   end
 
+  add_foreign_key "pictures", "users"
 end
