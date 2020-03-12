86a8b654b3a9a788ac5e6669fad3146907aa71a7
junichi-fujita <fujita0909j@gmail.com>
2020-03-12 20:59:54 +0900

    メールサーバーのホスト先を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'hogehoge-fugafuga.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'intense-garden-96385.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
